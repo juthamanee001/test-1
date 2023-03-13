39594def2384a88d807e69e04b11ddadfe6712df
juthamanee001 <123538000+juthamanee001@users.noreply.github.com>
2023-03-13 17:27:49 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args){
+        Console.Write("num");
+        int n = int.parse(Console.ReadLine());
+
+        Console.Write("day :");
+        string o = (Console.ReadLine());
+
+        if(n==o){
+            int 1 == monday;
+            int 2 == tuesday;
+            int 3 == wendnesday;
+            int 4 == thuesday;
+            int 5 == friday;
+            int 6 == saturday;
+            int 7 == sunday;
+        
+    }
+}
